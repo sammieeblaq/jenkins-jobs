bb7d4b96afbf5e13ded08aba03fe3da72ec712e3
sammieeblaq <odianoseairehrour@gmail.com>
2019-06-08 11:30:05 +0100

    changed name dict

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import sqlite3
 from datetime import datetime
 
 # defining jenkins parameters
-jenkins_url = "https://localhost:3000"
+jenkins_url = "http://172.82.162.36:8080"
 username = "samuel"
 password = "sammieeblaq"
 
@@ -13,24 +13,25 @@ db_name = "jenkins.db"
 #  get server instance
 server = Jenkins(jenkins_url, username, password)
 
-#  dictionary that holds the jobs name as keys 
-dict = {}
+# dictionary that holds the jobs name as keys 
+jobs_dict = {}
 
+# Connect to the database
 conn = sqlite3.connect(db_name)
 c = conn.cursor()
 
 for job_name, job_instance in server.get_jobs():
 	if job_instance.is_running():
-		status = 'RUNNING'
+		status = "Running"
 	elif job_instance.get_last_build_or_none() == None :
-		status = 'NOTBUILT'
+		status = "NotBuilt"
 	else:
 		simple_job = server.get_job(job_instance.name)
 		simple_build = simple_job.get_last_build()
 		status = simple_build.get_status()
 		
 	i = datetime.now()
-	checked_time = i.strftime('%Y/%m/%d %H:%M:%S')
+	checked_time = i.strftime("%Y/%m/%d %H:%M:%S")
 	tuple1 = (job_instance.name, status, checked_time)
 	c.execute("SELECT id FROM jenkins WHERE job_name = ?", (job_instance.name,))
 	data=c.fetchone()
@@ -41,7 +42,7 @@ for job_name, job_instance in server.get_jobs():
 		c.execute('UPDATE jenkins SET status=?, date_checked=? WHERE job_name=?', tuple2)
 		
 	### Add to dictionary ###
-	dict[job_instance.name] = status
+	jobs_dict[job_instance.name] = status
 	
 # Save (commit) the changes
 conn.commit()
